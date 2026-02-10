02f54e7b19c376ddbcee4153f1eb521df394140c
PK <paloma@palomakop.tv>
2026-02-09 23:10:42 -0500

    more newsletter display tweaks

--- src/newsletter/latest.11tydata.js
+++ src/newsletter/latest.11tydata.js
@@ -1,6 +1,6 @@
 export default {
   eleventyComputed: {
     thumbnail: (data) => data.buttondown?.image,
-    publishDate: (data) => data.buttondown?.publish_date ? new Date(data.buttondown.publish_date) : new Date()
+    publishDate: (data) => data.buttondown?.publish_date
   }
 };
