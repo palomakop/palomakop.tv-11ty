70bf2938f4fef676a7dbc8abc4ec53b8958bf728
PK <paloma@palomakop.tv>
2026-02-18 18:11:08 -0500

    make update pages use update image as thumbnail if present

--- src/update.11tydata.js
+++ src/update.11tydata.js
@@ -1,5 +1,6 @@
 export default {
   eleventyComputed: {
-    title: (data) => data.update?.date ? `${data.update.date} Update` : "Update"
+    title: (data) => data.update?.date ? `${data.update.date} Update` : "Update",
+    thumbnail: (data) => data.update?.image ?? data.thumbnail
   }
 };
